e932c2e31bae6726ae5a7aa055c22cc37fa2f335
yongfengnice <2499522170@qq.com>
2016-11-27 20:43:09 +0800

    添加快速向左滑动返回页面的处理

--- app/src/main/java/com/swipefinish/helper/SwipeHelper.java
+++ app/src/main/java/com/swipefinish/helper/SwipeHelper.java
@@ -13,6 +13,7 @@ public class SwipeHelper {
 
     private float mDownX;
     private float mMoveX;
+    private float mMaxMoveX;
     private long mDownTime;
     private int mSlideX = 30;
 
@@ -49,6 +50,7 @@ public class SwipeHelper {
             case MotionEvent.ACTION_MOVE:
                 if (mDownX > mSlideX) return;//按下边缘位置才允许滑动
                 mMoveX = ev.getX();
+                if (mMoveX > mMaxMoveX) mMaxMoveX = mMoveX;
                 if (mMoveX > (screenWidth - mSlideX / 2)) {
                     preContentView.setX(0);
                     curActivity.finish();
@@ -64,17 +66,19 @@ public class SwipeHelper {
                 break;
             case MotionEvent.ACTION_UP:
                 if (mDownX > mSlideX) return;//按下边缘位置才允许滑动
-                preContentView.setX(0);
                 float upX = ev.getX();
-                if ((upX - mDownX) / (System.currentTimeMillis() - mDownTime) > 0.5) {//快速滑动
+                if (upX < mMaxMoveX && (mMaxMoveX - upX) / (System.currentTimeMillis() - mDownTime) > 0.1) {//快速向左滑动
+                    curContentView.setX(0);
+                } else if ((upX - mDownX) / (System.currentTimeMillis() - mDownTime) > 0.5) {//快速向右滑动
                     curActivity.finish();
                 } else if (upX > screenWidth / 2) {//大于屏幕一半就结束当前Activity，根据自己需求而定吧
                     curActivity.finish();
                 } else {
                     curContentView.setX(0);
                 }
+                preContentView.setX(0);
+                mMaxMoveX = 0;
                 break;
         }
     }
-
 }
